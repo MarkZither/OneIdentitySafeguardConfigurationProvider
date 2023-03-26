cdfc9766d03359927053cdf841a72ef3ceb39397
MarkZither <mark.burton@zither-it.co.uk>
2023-03-26 16:20:59 +0200

    add ConfigurationBuilderExtensions

---
+++ src/MarkZither.Extensions.Configuration.OneIdentitySafeguard/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MarkZither.Extensions.Configuration.OneIdentitySafeguard;
+
+using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Configuration;
+public static class ConfigurationBuilderExtensions {
+    public static IConfigurationBuilder AddEntityConfiguration(
+    this IConfigurationBuilder builder) {
+        var tempConfig = builder.Build();
+
+        return builder.Add(new OneIdentitySafeguardConfigurationSource());
+    }
+}
