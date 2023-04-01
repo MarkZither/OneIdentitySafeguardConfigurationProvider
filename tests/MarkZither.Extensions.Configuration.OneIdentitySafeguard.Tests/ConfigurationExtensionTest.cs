3b5d24faf8b51122d2da7f060fb3f75a7e8cbac7
MarkZither <mark.burton@zither-it.co.uk>
2023-04-01 22:23:23 +0200

    correct formatting

--- tests/MarkZither.Extensions.Configuration.OneIdentitySafeguard.Tests/ConfigurationExtensionTest.cs
+++ tests/MarkZither.Extensions.Configuration.OneIdentitySafeguard.Tests/ConfigurationExtensionTest.cs
@@ -2,8 +2,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace MarkZither.Extensions.Configuration.OneIdentitySafeguard.Tests;
 
-public class ConfigurationExtensionTest
-{
+public class ConfigurationExtensionTest {
     [Fact]
     public void AddUserSecrets_Does_Not_Fail_On_Non_Existing_File() {
         var builder = new ConfigurationBuilder().AddOneIdentitySafeguardConfiguration();
